7808395d9caf019429d1ea50e6887782b9100bf5
shenjianlin <153334319@qq.com>
2017-04-10 20:24:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mybatis 该项目 作为mybatis 演示
+
+mybatis 测试示例代码 自己学习使用
+
+1.tag1 ： 项目初始 v1.0
---
+++ src/main/java/com/fengze/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.fengze.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
